4ab23c3f9fc607c0c9ca8750311a538c40c32150
Shelton Victor Coutinho <shelvic10@gmail.com>
2026-03-09 02:05:35 +0530

    Used Quarteneries to avoid camera lock

--- include/Camera.h
+++ include/Camera.h
@@ -1,8 +1,10 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 enum class CameraMovement {
     FORWARD,
@@ -27,6 +29,7 @@ public:
     void processKeyboard(CameraMovement direction, float deltaTime);
     void processMouseMovement(float xOffset, float yOffset, bool constrainPitch = true);
     void processMouseScroll(float yOffset);
+    void processOrbit(float xOffset, float yOffset, const glm::vec3& pivot);
 
     glm::vec3 getPosition() const { return m_position; }
     glm::vec3 getFront() const { return m_front; }
@@ -37,6 +40,8 @@ public:
     float getMouseSensitivity() const { return m_mouseSensitivity; }
     float getNearPlane() const { return m_nearPlane; }
     float getFarPlane() const { return m_farPlane; }
+    glm::quat getOrientation() const { return m_orientation; }
+    void setOrientation(const glm::quat& q) { m_orientation = q; updateCameraVectors(); }
 
     void setPosition(const glm::vec3& position) { m_position = position; }
     void setMovementSpeed(float speed) { m_movementSpeed = speed; }
@@ -47,14 +52,16 @@ public:
     void lookAt(const glm::vec3& target);
 
 private:
+
     glm::vec3 m_position;
+    glm::quat m_orientation;
     glm::vec3 m_front;
     glm::vec3 m_up;
     glm::vec3 m_right;
     glm::vec3 m_worldUp;
 
-    float m_yaw;
-    float m_pitch;
+    float m_yaw;   // for UI only
+    float m_pitch; // for UI only
 
     float m_movementSpeed;
     float m_mouseSensitivity;
--- include/CameraState.h
+++ include/CameraState.h
@@ -1,12 +1,15 @@
 #ifndef CAMERA_STATE_H
 #define CAMERA_STATE_H
 
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <string>
 
 struct CameraState {
     std::string name;
     glm::vec3 position;
+    glm::quat orientation;
     float yaw;
     float pitch;
     float fov;
